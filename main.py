45570911fa880e7aff28668665d08c6f83208468
piwniczak2137 <70859223+piwniczak2137@users.noreply.github.com>
2022-11-06 11:11:06 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from discord import *
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+client = Client(intents=Intents.default())
+
+@client.event
+def on_message(message):
+   pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+client.run(os.getenv("TOKEN"))
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
